8ca89d4f4093b61316c1300c84ac38cf1a5f9fda
jon <jonjuarez.contacto@gmail.com>
2025-10-26 12:35:50 +0100

    Ppner enlace en el header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
     <header className='bg-card text-card-foreground rounded-xl border p-3 shadow-sm flex sticky items-center gap-3 mb-8' >
         <span className='icon'>💻</span>
         <h1 className='text-lg font-bold leading-tight tracking-[-0.015em]'>tendenciasDev</h1>
-        <Button variant='ghost' size='sm' className='ml-auto'>Creado por Jon Juárez</Button>
+        <Button variant='ghost' size='sm' className='ml-auto'><a href="#" >Creado por Jon Juárez</a></Button>
     </header>
   )
 }
