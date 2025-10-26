7d6c6189fd96cc0fa41000d5d87f6f654577bddf
jon <jonjuarez.contacto@gmail.com>
2025-10-26 13:34:24 +0100

    Arreglar la cagada de portfolio que deberia ser tendencias Dev

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { Button } from './ui/button'
 const Header = () => {
   return (
     <header className='bg-card text-card-foreground rounded-xl border p-3 shadow-sm flex sticky items-center gap-3 mb-8' >
-        <span className='icon'><img src="./tendeciasDev.png" alt="Tendencias Dev" /></span>
+        <span className='w-[64px]'><img src="src/assets/img/tendenciasDev.png" alt="Tendencias Dev" /></span>
         <h1 className='text-lg font-bold leading-tight tracking-[-0.015em]'>tendenciasDev</h1>
         <Button variant='ghost' size='sm' className='ml-auto'><a href="#" >Creado por Jon Juárez</a></Button>
     </header>
