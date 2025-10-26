64c9f08574baf5f31a9cd3f50a2b688cc305fefc
jon <jonjuarez.contacto@gmail.com>
2025-10-26 12:32:25 +0100

    Header terminado titulo y action

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+
+const Header = () => {
+  return (
+    <header className='bg-card text-card-foreground rounded-xl border p-3 shadow-sm flex sticky items-center gap-3 mb-8' >
+        <span className='icon'>💻</span>
+        <h1 className='text-lg font-bold leading-tight tracking-[-0.015em]'>tendenciasDev</h1>
+        <Button variant='ghost' size='sm' className='ml-auto'>Creado por Jon Juárez</Button>
+    </header>
+  )
+}
+
+export default Header
